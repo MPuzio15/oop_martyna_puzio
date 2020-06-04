1268055002371dedf26d8b156d1814de657d037c
martyna <martynapuzio93@gmail.com>
2020-06-04 21:52:30 +0200

    second commit

---
+++ zad_10/zad_10.py
@@ -0,0 +1,14 @@
+'''
+Zadanie 10: [KOD]   Mając poniższy kod spraw aby wywołanie funkcji użyło keyword argumentu (1 pkt).
+def funny_function(imie):
+	print(imie)
+
+funny_function("Mmmmrrp")
+
+'''
+
+def funny_function(imie="Krzys"):
+	print(imie)
+
+funny_function("Mmmmrrp")
+funny_function()
--- zad_9/zad_9.py
+++ zad_9/zad_9.py
@@ -2,7 +2,46 @@
 Zadanie 9:  [KOD] Do czego służy słowo kluczowe super.
 Napisz przykład jego użycia (2 pkt).
 
-Slowa kluczowego super() używamy do wywolania w klasie
-pochodnej metod z klasy nadrzednej.
+Slowa kluczowego super() używamy do wywolania w konstruktorze klasy
+pochodnej metody __init__ z klasy bazowej.
+Wskazujemy w ten sposób, które pola mają być odziedziczone, a dodatkowo możemy
+zadeklarować dodatkowe pola w danym konstruktorze, które nie beda dostepne np wklasie bazowej,
+ale juz w klasie pochodnej danej klasy bedziemy mieli do nich dostep przy uzyciu super(), np:
 '''
 
+
+class Person:
+    def __init__(self, name, lastname, age):
+        self.name = name
+        self.lastname = lastname
+        self.age = age
+
+    def print_out_details(self):
+        print(f'I am {self.name} and I am {self.age}')
+
+
+class Employee(Person):
+    def __init__(self, name, lastname, age, id):
+        super().__init__(name, lastname, age)
+        self.id = id
+
+    def print_out_details(self):
+        print(f'I am {self.name} and I am {self.age}. My Id is{self.id}')
+
+
+class HrEmployee(Employee):
+    def __init__(self, name, lastname, age, id, number_of_interviews_arranged):
+        super().__init__(name, lastname, age, id)
+        self.number_of_interviews_arranged = number_of_interviews_arranged
+
+    def print_out_details(self):
+        print(
+            f'I am {self.name} and I am {self.age}.\nMy id is {self.id}. I have arranged {self.number_of_interviews_arranged}')
+
+
+osoba = Person("Jan", "Kowalski", 50)
+pracownik = Employee("Adam", "Kot", 15, 123456)
+hrowiec = HrEmployee("Piotr", "Lis", 34, 34256354, 12)
+osoba.print_out_details()
+pracownik.print_out_details()
+hrowiec.print_out_details()
